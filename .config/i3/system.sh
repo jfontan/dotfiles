91ed1bc8799be75deb745a70500226da8b23c894
Javi Fontan <jfontan@opennebula.org>
2016-06-29 20:45:38 +0200

    Changes in screen configuration

--- .config/i3/system.sh
+++ .config/i3/system.sh
@@ -6,15 +6,13 @@ shutdown=" shutdown"
 solo=" solo"
 home=" home"
 office=" office"
-milieu=" milieu"
 
 options="$lock
 $suspend
 $shutdown
 $solo
 $home
-$office
-$milieu"
+$office"
 
 opt=$(echo "$options" | rofi -dmenu -font "hermit 40" -sidebar-mode)
 
@@ -39,9 +37,6 @@ case $opt in
         ;;
     $office)
         ~/.screenlayout/oficina.sh
-        ;;
-    $milieu)
-        ~/.screenlayout/milieu.sh
         ;;
 esac
 
--- .screenlayout/oficina.sh
+++ .screenlayout/oficina.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI1 --off --output DP1 --mode 2560x1440 --pos 1920x0 --rotate normal --output eDP1 --mode 1920x1080 --pos 2288x1440 --rotate normal --output VIRTUAL1 --off
+xrandr --output HDMI1 --off --output DP1 --mode 2560x1440 --pos 0x0 --rotate normal --output eDP1 --primary --mode 1920x1080 --pos 336x1440 --rotate normal --output VIRTUAL1 --off
